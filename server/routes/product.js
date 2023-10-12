03ca9de1d615de279298ff3b00a19cd00ff3eec5
Ibrahimfathi96 <himafathy796@gmail.com>
2023-10-12 23:40:33 +0300

    Home Categories

--- server/routes/product.js
+++ server/routes/product.js
@@ -5,7 +5,7 @@ const Product = require("../models/product");
 
 productRouter.get("/api/products", auth, async (req, res) => {
   try {
-    console.log("catergory", req.query.catergory);
+    console.log("category", req.query.category);
     const products = await Product.find({ category: req.query.category });
     res.json(products);
   } catch (error) {
